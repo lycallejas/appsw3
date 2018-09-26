19b345674eaa19d2c985d922eacec8f74ed9a9ae
lycallejas <laurita.c2008@gmail.com>
2018-09-25 22:29:33 -0500

    .

---
+++ ProyectoSf3/app/app/src/main/java/juansebastian/android/uniquindio/edu/co/proyectosf3/activity/VentanaRegistro.kt
@@ -0,0 +1,13 @@
+package juansebastian.android.uniquindio.edu.co.proyectosf3.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import juansebastian.android.uniquindio.edu.co.proyectosf3.R
+
+class VentanaRegistro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana_registro)
+    }
+}
---
+++ ProyectoSf3/app/app/src/main/java/juansebastian/android/uniquindio/edu/co/proyectosf3/util/constantes.kt
@@ -0,0 +1,5 @@
+package juansebastian.android.uniquindio.edu.co.proyectosf3.util
+
+const val REGISTRO:String=" RESGISTRO"
+const val INVITADO:String=" INVITADO"
+const val USUARIO:String=" USUARIO"
